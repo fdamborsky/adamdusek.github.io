03e8e8615a223066e352bd28fa73b300d7ab769a
fdamborsky <155442035+fdamborsky@users.noreply.github.com>
2025-01-08 20:22:23 +0100

    Patch

--- newsletter/podekovani/script.js
+++ newsletter/podekovani/script.js
@@ -5,9 +5,9 @@ let paragraphNeW = `
                 <img src="resources/imgs/download.webp" alt="">
             </h1>
             <p>E-mailová schránka je někdy chaos.</p>
-            <p>E-mail, který si chceš přečíst, může omylem skončit ve složce SPAM nebo v kartě Propagace.</p>
-            <p>A ty tak přijdeš o hodnotné lekce z chytrých knížek. <img src="resources/imgs/skull.webp" alt=""></p>
-            <p>Abys měl jistotu, že ode mě budeš pravidelně dostávat všechen obsah, postupuj podle těchto jednoduchých kroků:</p>
+            <p>E-mail, který si chceš přečíst, může&nbsp;omylem skončit ve složce SPAM&nbsp;nebo v kartě Propagace.</p>
+            <p>A ty tak přijdeš o hodnotné lekce z&nbsp;chytrých knížek. <img src="resources/imgs/skull.webp" alt=""></p>
+            <p>Abys měl jistotu, že ode mě budeš pravidelně dostávat všechen obsah,&nbsp;postupuj podle těchto jednoduchých kroků:</p>
             `
 
 // Function to check window width and update paragraph
